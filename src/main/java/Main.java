12cb461ed73e987b19431c77043d5094ae893384
Adrian Khaskin <haskin.dev@gmail.com>
2022-07-19 10:31:32 -0400

    Added implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,21 @@
+/*
+ * Problem:
+ * - Create a functional interface called ConcatStringFunction that has a method called apply that takes 
+ * in a variable amount of String arguments and returns a String values.
+ */
+
 public class Main {
     public static void main(String[] args) {
         ConcatStringFunction func = str -> String.join("", str).toUpperCase();
         String res = func.apply("banana", "grape", "smoothie");
         System.out.println(res); // BANANAGRAPESMOOTHIE
 
-				String res2 = func.apply("apple", "orange", "pineapple", "smoothie");
-				System.out.println(res); // APPLEORANGEPINEAPPLESMOOTHIE
+        String res2 = func.apply("apple", "orange", "pineapple", "smoothie");
+        System.out.println(res2); // APPLEORANGEPINEAPPLESMOOTHIE
     }
 }
 
 @FunctionalInterface
-// your code here
+interface ConcatStringFunction {
+    String apply(String... strings);
+}
